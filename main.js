9cdf1bbd9663ce476a2d4434a33d002df7e2fe54
João Pedro Reis <joaoreis.pedro@estudante.ufjf.br>
2023-12-19 00:22:03 -0300

    alteraçoes no index e main

--- main.js
+++ main.js
@@ -1,9 +1,49 @@
-
-function jogadorDois(){
+function jogadorDois() {
     let casaRandom = Math.floor(Math.random() * 10);
-    let dadoRandom = math.floor(math.random() * 7);
-
-    
-
-
-}
+  
+    switch (casaRandom) {
+      case 0:
+        let dadoRandom = Math.floor(Math.random() * 7);
+        
+        break;
+      case 1:
+         dadoRandom = Math.floor(Math.random() * 7);
+        // Faça algo com casaRandom e dadoRandom quando casaRandom for 1
+        break;
+      case 2:
+         dadoRandom = Math.floor(Math.random() * 7);
+        // Faça algo com casaRandom e dadoRandom quando casaRandom for 2
+        break;
+      case 3:
+         dadoRandom = Math.floor(Math.random() * 7);
+        // Faça algo com casaRandom e dadoRandom quando casaRandom for 3
+        break;
+      case 4:
+         dadoRandom = Math.floor(Math.random() * 7);
+        // Faça algo com casaRandom e dadoRandom quando casaRandom for 4
+        break;
+      case 5:
+         dadoRandom = Math.floor(Math.random() * 7);
+        // Faça algo com casaRandom e dadoRandom quando casaRandom for 5
+        break;
+      case 6:
+         dadoRandom = Math.floor(Math.random() * 7);
+        // Faça algo com casaRandom e dadoRandom quando casaRandom for 6
+        break;
+      case 7:
+         dadoRandom = Math.floor(Math.random() * 7);
+        // Faça algo com casaRandom e dadoRandom quando casaRandom for 7
+        break;
+      case 8:
+         dadoRandom = Math.floor(Math.random() * 7);
+        // Faça algo com casaRandom e dadoRandom quando casaRandom for 8
+        break;
+      case 9:
+         dadoRandom = Math.floor(Math.random() * 7);
+        // Faça algo com casaRandom e dadoRandom quando casaRandom for 9
+        break;
+      default:
+        // Faça algo se casaRandom não corresponder a nenhum caso
+    }
+  }
+  
