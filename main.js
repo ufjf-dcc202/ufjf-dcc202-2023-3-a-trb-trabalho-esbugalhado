46f1a03f94c1409ebd44255311af5ecad9a44c0b
João Pedro Reis <joaoreis.pedro@estudante.ufjf.br>
2023-12-19 19:32:49 -0300

    ultimas alteraçoes no main

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+const celulas = [
+  "cell1b1",
+  "cell2b1",
+  "cell3b1",
+  "cell4b1",
+  "cell5b1",
+  "cell6b1",
+  "cell7b1",
+  "cell8b1",
+  "cell9b1"
+]
+
+function clicarCelula(celula, jogador = true) {
+  const casa = document.getElementById(celula);
+
+  if (casa.children.length > 0) {
+    return false;
+  }
+
+  const dado = selecionaDado();
+  let img = document.createElement("img");
+  img.setAttribute("src", dado);
+  img.setAttribute("width", "100px");
+
+  casa.appendChild(img);
+  return true;
+
+}
+
+function jogada(celula) {
+  const validacaoJogada = clicarCelula(celula);
+  if(validacaoJogada) jogadaMaquina();
+}
+
+function jogadaMaquina() {
+
+  let casaID = Math.floor(Math.random() * 9);
+  let validarJogada = clicarCelula(celulas[casaID], false)
+  while(validarJogada == false){
+    casaID = Math.floor(Math.random() * 9);
+    validarJogada = clicarCelula(celulas[casaID], false)
+  };
+}
+
 function selecionaDado() {
   let dadoRandom = Math.floor(Math.random() * 7);
   switch (dadoRandom) {
